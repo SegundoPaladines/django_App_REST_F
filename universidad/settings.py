afb54d7964e18734c23725e679ae4a17810cddb2
SegundoPaladines <segundopaladines@gmail.com>
2023-11-05 14:42:12 -0500

    estilos facultades

--- universidad/settings.py
+++ universidad/settings.py
@@ -102,6 +102,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+#Url para logearse
+LOGIN_URL = '/admin'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
--- universidad/urls.py
+++ universidad/urls.py
@@ -17,26 +17,40 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from university import views
+from django.contrib.auth.decorators import login_required #para que toque authenticarse
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.UniversidadView.as_view(), name='udenar'), # Todas las facultades
+    
+    #no requieren autenticacion
+    #bienvenida
+    path('', views.UniversidadView.as_view(), name='udenar'), 
+    
+    #Facultades
     path('universidad/facultades/', views.FacultadesListView.as_view(), name='lista-facultades'), #detalle de la facultad
     path('facultad/<int:pk>/detail/',
          views.FacultadDetailView.as_view(), name='fac-detail'),
-    path('facultad/<int:pk>/update/',views.FacultadUpdate.as_view(), name='fac-update'), # Editar facultad
-    path('facultad/<int:pk>/delete/', views.FacultadDelete.as_view(), name='fac-delete'), # ELiminar Facultad
-    path('facultad/create/', views.FacultadCreate.as_view(), name='fac-create'), # Crear Facultad
+    
+    # Programas
     path('universidad/programas/', views.ProgramasListView.as_view(), name='lista-programas'), #Todos los programas
     path('programa/<int:pk>/detail/',
             views.ProgramaDetailView.as_view(), name='prog-detail'),
-    path('programa/<int:pk>/update/',views.ProgramaUpdate.as_view(), name='prog-update'), # Editar programa
-    path('programa/<int:pk>/delete/', views.ProgramaDelete.as_view(), name='prog-delete'), # ELiminar programa
-    path('programa/create/', views.ProgramaCreate.as_view(), name='prog-create'), # Crear programa
+    
+    # docentes
     path('universidad/docentes/', views.DocentesListView.as_view(), name='lista-docentes'), #todos los docentes
     path('docente/<int:pk>/detail/',
             views.DocenteDetailView.as_view(), name='doc-detail'),
-    path('docente/<int:pk>/update/',views.DocenteUpdate.as_view(), name='doc-update'), # Editar docente
-    path('docente/<int:pk>/delete/', views.DocenteDelete.as_view(), name='doc-delete'), # ELiminar docente
-    path('docente/create/', views.DocenteCreate.as_view(), name='doc-create'), # Crear docente
+
+    #las que requieren autenticacion
+    path('facultad/<int:pk>/update/', login_required(views.FacultadUpdate.as_view()), name='fac-update'),  # Editar facultad
+    path('facultad/<int:pk>/delete/', login_required(views.FacultadDelete.as_view()), name='fac-delete'),  # Eliminar facultad
+    path('facultad/create/', login_required(views.FacultadCreate.as_view()), name='fac-create'),  # Crear facultad
+
+    path('programa/<int:pk>/update/', login_required(views.ProgramaUpdate.as_view()), name='prog-update'),  # Editar programa
+    path('programa/<int:pk>/delete/', login_required(views.ProgramaDelete.as_view()), name='prog-delete'),  # Eliminar programa
+    path('programa/create/', login_required(views.ProgramaCreate.as_view()), name='prog-create'),  # Crear programa
+
+    path('docente/<int:pk>/update/', login_required(views.DocenteUpdate.as_view()), name='doc-update'),  # Editar docente
+    path('docente/<int:pk>/delete/', login_required(views.DocenteDelete.as_view()), name='doc-delete'),  # Eliminar docente
+    path('docente/create/', login_required(views.DocenteCreate.as_view()), name='doc-create'),  # Crear docente
 ]
