763052bf1979ec931ed03b509b3f0e24b9ff2be0
SegundoPaladines <segundopaladines@gmail.com>
2023-11-14 01:21:16 -0500

    vista de bienvenida

--- universidad/settings.py
+++ universidad/settings.py
@@ -130,4 +130,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'media'),
+    os.path.join(BASE_DIR, 'university/templates/img/')
 )
